e097f64eda0bf0dd20913e0614a6215d1f5b3d0a
Steven Frost <steve.frost.private@gmail.com>
2014-03-19 17:10:40 +0000

    Completed surface-surface collision testing

--- src/Collision.cpp
+++ src/Collision.cpp
@@ -1,26 +1,26 @@
 #include "Collision.h"
+#include <algorithm>
+
+using std::max;
+using std::min;
 
 bool Collision::boundingBox(DisplayableObject *a, DisplayableObject *b) {
-	static unsigned int count = 0;
-
+	/* Get the object rectangles */
 	SDL_Rect aRect, bRect;
-	
 	a->GetRedrawRect(&aRect);
 	b->GetRedrawRect(&bRect);
 
+	/* Calculate their right, left and bottom positions */
 	int aRight  = aRect.x + aRect.w;
 	int aLeft   = aRect.x;
-	int aTop    = aRect.y;
 	int aBottom = aRect.y + aRect.h;
-
 	int bRight  = bRect.x + bRect.w;
 	int bLeft   = bRect.x;
-	int bTop    = bRect.y;
 	int bBottom = bRect.y + bRect.h;
 
+	/* Check if any positions overlap */
 	if ((aLeft <= bRight && aRight >= bLeft) || (aLeft >= bRight && aRight <= bLeft)) {
-		if (aTop <= bBottom && aBottom >= bTop) {
-			printf("COLLISION {%d}\r\n", count++);
+		if (aRect.y <= bBottom && aBottom >= bRect.y) {
 			return true;
 		}
 	}
@@ -28,13 +28,27 @@ bool Collision::boundingBox(DisplayableObject *a, DisplayableObject *b) {
 	return false;
 }
 
-bool Collision::surface(SDL_Surface *a, SDL_Surface *b) {
-	for (int x = 0; x < a->w; x++) {
-		for (int y = 0; x < a->h; y++) {
+bool Collision::surface(int resolution, SDL_Surface *a, SDL_Surface *b, int aX, int aY, int bX, int bY) {
+	/* Calculate the intersection of the two surfaces */
+	int left	= max(aX, bX);
+	int top		= max(aY, bY);
+	int right	= min(aX + a->w, bX + b->w);
+	int bottom	= min(aY + a->h, bY + b->h);
 
+	for (int x = left; x < right; x += resolution) {
+		for (int y = top; y < bottom; y += resolution) {
+			/* Calculate the array index of the pixel */
+			int aIndex = (x - aX) + (y - aY) * a->pitch / sizeof(unsigned int);
+			int bIndex = (x - bX) + (y - bY) * b->pitch / sizeof(unsigned int);
+
+			/* Get the pixel alpha data */
+			unsigned int aPixel = (static_cast<unsigned int *>(a->pixels)[aIndex]) & 0xFF000000;
+			unsigned int bPixel = (static_cast<unsigned int *>(b->pixels)[bIndex]) & 0xFF000000;
+
+			/* If both alphas are non-zero, we have a collision */
+			if (aPixel != 0x00000000 && bPixel != 0x00000000) return true;
 		}
 	}
 
-	//unsigned int *aPixel = ((unsigned int *)a->pixels)[iX + iY * pTarget->pitch / sizeof(unsigned int)]
 	return false;
 }
--- src/Collision.h
+++ src/Collision.h
@@ -5,8 +5,36 @@
 
 class Collision {
 public:
+	/**
+	* Performs a fast bounding box collision test on two displayable objects.
+	* This function can be used for approximation of collisions, before a more
+	* CPU-intensive collision test.
+	*
+	* @param a	the first surface
+	* @param b	the second surface
+	* @return	true if the boxes intersect
+	*/
 	static bool boundingBox(DisplayableObject *a, DisplayableObject *b);
-	static bool surface(SDL_Surface *a, SDL_Surface *b);
+
+	/**
+	* Performs a slower collision test on two SDL surfaces. The test speed can
+	* be increased by using a less precise check (higher resolution parameter
+	* value).
+	*
+	* @precondition		the two surfaces are overlapping
+	* @precondition		the surfaces are normalised
+	*
+	* @param resolution	the resolution of the collision check, in pixels
+	* @param a			the first surface
+	* @param b			the second surface
+	* @param aX			first surface x-coordinate (world space)
+	* @param aY			first surface y-coordinate (world space)
+	* @param bX			second surface x-coordinate (world space)
+	* @param bY			second surface y-coordinate (world space)
+	* 
+	* @return			true if a collisioin occured at the pixel resolution level
+	*/
+	static bool surface(int resolution, SDL_Surface *a, SDL_Surface *b, int aX, int aY, int bX, int bY);
 };
 
 #endif // !COLLISION_H
